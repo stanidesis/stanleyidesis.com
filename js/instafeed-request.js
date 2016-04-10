c24258896e1611b1d73921f8091bc30001fd7a29
Stanley <stan.idesis@gmail.com>
2016-04-10 17:11:14 +0000

    Instagram works

---
+++ js/instafeed-request.js
@@ -0,0 +1,11 @@
+var userFeed = new Instafeed({
+    get: 'user',
+    userId: '182831931',
+    clientId: '467ede5a6b9b48ae8e03f4e2582aeeb3',
+    resolution: 'thumbnail',
+    sortBy: 'most-recent',
+    limit: 9 * 2,
+    target: 'instagram-tiles',
+    template: '<div class="instagram-tile"><div class="instagram-overlay"></div><a href="{{link}}"><img src="{{image}}"/></a></div>'
+  });
+userFeed.run();
