f4914271b9cc3aed9659da6c40881129cc2d1ab5
Stanley Idesis <stan.idesis@gmail.com>
2016-06-09 21:05:47 +0000

    Content Blocking Optimizations
    I attempted to lazyload CSS assets and made
    asynchronous the required scripts as well as
    in-lining some small scripts that did not
    require asynchronous loading.
    
    Future optimizations should include moving all
    assets to a CDN (Google Buckets) and of course
    compressing every asset.

--- js/chrome-android-fix.js
+++ js/chrome-android-fix.js
@@ -1,23 +1,21 @@
 function greedyJumbotron(jumbotron) {
-    // HAAXX
-    if (screen.width >= 768) {
-        jumbotron.height = 500;
-        return;
+  // HAAXX
+  if (screen.width >= 768) {
+    jumbotron.height = 500;
+    return;
+  }
+  // Approximately URL bar height in Chrome on tablet
+  var HEIGHT_CHANGE_TOLERANCE = 100;
+  var viewportHeight = screen.height;
+  window.onresize = function () {
+    if (Math.abs(viewportHeight - screen.height) > HEIGHT_CHANGE_TOLERANCE) {
+      viewportHeight = screen.height;
+      update();
     }
-    var HEIGHT_CHANGE_TOLERANCE = 100; // Approximately URL bar height in Chrome on tablet
+  };
 
-    var viewportHeight = screen.height;
-
-    window.onresize = function () {
-        if (Math.abs(viewportHeight - screen.height) > HEIGHT_CHANGE_TOLERANCE) {
-            viewportHeight = screen.height;
-            update();
-        }
-    }
-
-    function update() {
-        jumbotron.style.height = viewportHeight + 'px';
-    }
-
-    update();
+  function update() {
+    jumbotron.style.height = viewportHeight + 'px';
+  }
+  update();
 }
