c122fb2437e06c4ffa2082c50ea65645ff65cea2
Stanley Idesis <stan.idesis@gmail.com>
2018-09-29 19:14:22 -0500

    Touch Ups to the Homepage

--- _includes/home.js
+++ _includes/home.js
@@ -1,3 +1,6 @@
 window.addDependencyCallback(function() {
   window.setupBookModal();
+  if ($('#talents-level p').first().is(':hidden')) {
+    loadjs('https://unpkg.com/tippy.js@3/dist/tippy.all.min.js');
+  }
 });
